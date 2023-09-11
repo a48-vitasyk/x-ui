49936caab89500079933df542c841765d3a12937
a48-vitasyk <131271390+a48-vitasyk@users.noreply.github.com>
2023-09-11 10:49:31 +0300

    Update x-ui.sh
    fix

--- x-ui.sh
+++ x-ui.sh
@@ -1,42 +1,34 @@
 #!/bin/bash
 
-# Функция для вывода текста красным цветом
 print_red() {
     echo -e "\e[31m$1\e[0m"
 }
 
-# Функция для вывода текста зелёным цветом
 print_green() {
     echo -e "\e[32m$1\e[0m"
 }
 
-# Установка необходимых зависимостей
 apt-get update -qq > /dev/null
 apt-get install -y -qq ca-certificates curl gnupg vim > /dev/null
 
-# Настройка репозитория Docker
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# Установка Docker и плагинов
 apt-get update -qq > /dev/null
 apt-get install -y -qq docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin > /dev/null
 systemctl enable docker
 
-# Установка Docker Compose
 curl -SL https://github.com/docker/compose/releases/download/v2.20.3/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-# Вывод версий Docker и Docker Compose
 docker_version=$(docker --version)
 docker_compose_version=$(docker-compose --version)
 print_red "Установленная версия Docker: $docker_version"
 print_red "Установленная версия Docker Compose: $docker_compose_version"
 
-# Создание файла docker-compose.yaml для X-UI
 mkdir -p /home/x-ui
 cat <<EOL > /home/x-ui/docker-compose.yaml
 version: "3.9"
@@ -54,11 +46,9 @@ services:
     restart: unless-stopped
 EOL
 
-# Запуск контейнера X-UI
 cd /home/x-ui
 docker-compose up -d
 
-# Вывод логов с информацией о порте
 sleep 5
 log_output=$(docker logs x-ui 2>&1 | grep "INFO - web server run http on")
 print_green "$log_output"
